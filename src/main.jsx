082ac99f1392a698e1ce51ab9b620f08c6926070
Mora-net <rab210221@gmail.com>
2026-01-06 00:18:38 +0700

    updateHome

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,17 @@
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// import './index.css'
-// import App from './App.jsx'
-
-// createRoot(document.getElementById('root')).render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>,
-// )
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-// Bootstrap CSS
+// Bootstrap CSS & Icons
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 
 // Custom CSS
 import "./assets/css/style.css";
 import "./assets/css/aos.css";
+
+// Optional: custom JS
+import "./assets/js/script.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
